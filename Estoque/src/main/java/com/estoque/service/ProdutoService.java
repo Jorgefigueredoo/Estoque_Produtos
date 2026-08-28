fadf904a1ae29ad5b25cd4980c6aab8381a17e37
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-08-28 20:25:33 -0300

    Injetor ajustado, agora está indo pelo contrutor

--- Estoque/src/main/java/com/estoque/service/ProdutoService.java
+++ Estoque/src/main/java/com/estoque/service/ProdutoService.java
@@ -2,7 +2,6 @@ package com.estoque.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estoque.model.Produto;
